fc10e8ea58a47f81e40bbb337e8b34e673b04221
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-08 15:25:17 -0800

    Cleanup

--- BugKeeper/scripts/BugDex.cs
+++ BugKeeper/scripts/BugDex.cs
@@ -1,31 +1,29 @@
 using Godot;
 using System;
-
-public enum BugCaughtStatus {
-	Unseen,
-	Seen,
-	Caught
-}
-
-public enum BugActiveTimes {
-	Never = 0,
-	Morning = 1,
-	Day = 2,
-	Evening = 4,
-	Night = 8,
-	Always = 15
-}
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class BugDex : Node
 {
-	public override void _Ready()
+	public PackedScene BugDexEntryNode;
+	public List<BugDexEntry> BugDexEntries;
+	public override void _EnterTree()
 	{
-		BugDexEntry entry = new BugDexEntry();
-		entry.Name = "Big Bug";
-		GD.Print(entry.Name);
+		base._EnterTree();
+		BugDexEntryNode = ResourceLoader.Load<PackedScene>("res://scenes/bugDexEntry.tscn");
 	}
-	
-	public override void _Process(double delta)
+
+    public override void _Ready()
+    {
+        base._Ready();
+		foreach (var entry in BugDexEntries)
+		{
+			Node newEntryNode = BugDexEntryNode.Instantiate<Node>();
+		}
+    }
+
+    public override void _Process(double delta)
 	{
+		base._Process(delta);
 	}
 }
--- BugKeeper/scripts/BugDexEntry.cs
+++ BugKeeper/scripts/BugDexEntry.cs
@@ -1,32 +1,82 @@
 ﻿using Godot;
 using System;
 
-public class BugDexEntry {
-    public BugDexEntry() {
-        Name = "name";
-        Species = "species";
-        Description = "description";
-        Sprite = new Sprite2D();
-        ActiveTimes = BugActiveTimes.Never;
-        CaughtStatus = BugCaughtStatus.Unseen;
-        TimesCaught = 0;
-    }
+public enum BugCaughtStatus
+{
+    Unseen,
+    Seen,
+    Caught
+}
 
-    public BugDexEntry(string name, string species, string description, string spritePath, string activeTimes) {
-        Name = name;
-        Species = species;
-        Description = description;
-        Sprite = ResourceLoader.Load<Sprite2D>(spritePath);
-        ActiveTimes = Enum.Parse<BugActiveTimes>(activeTimes);
-        CaughtStatus = BugCaughtStatus.Unseen;
-        TimesCaught = 0;
-    }
+public enum BugSchedule
+{
+    Never = 0,
 
-    public string Name;
+    Morning = 1,
+    Day = 2,
+    Evening = 4,
+    Night = 8,
+
+    Diurnal = Morning | Day | Evening,
+    Nocturnal = Evening | Night | Morning,
+
+    Matutinal = Night | Morning | Day,
+    Vespertinal = Day | Evening | Night,
+
+    Crepuscular = Morning | Evening,
+    Biphasic = Day | Night,
+
+    Early = Morning | Day,
+    Late = Evening | Night,
+
+    Always = Morning | Day | Evening | Night,
+    Cathemeral = Always
+}
+
+public partial class BugDexEntry : Control
+{
+    [ExportGroup("Bug Information")]
+    [Export]
+    public string BugName;
+    [Export]
     public string Species;
+    [Export]
     public string Description;
+    [ExportGroup("")]
+    [Export]
     public Sprite2D Sprite;
-    public BugActiveTimes ActiveTimes;
+    [Export]
+    public BugSchedule Schedule;
+    [Export]
     public BugCaughtStatus CaughtStatus;
     public int TimesCaught;
+
+    //public BugDexEntry() {
+    //    BugName = "name";
+    //    Species = "species";
+    //    Description = "description";
+    //    Sprite = new Sprite2D();
+    //    Schedule = BugSchedule.Never;
+    //    CaughtStatus = BugCaughtStatus.Unseen;
+    //    TimesCaught = 0;
+    //}
+
+    //public BugDexEntry(string name, string species, string description, string spritePath, string schedule) {
+    //    BugName = name;
+    //    Species = species;
+    //    Description = description;
+    //    Sprite = ResourceLoader.Load<Sprite2D>(spritePath);
+    //    Schedule = Enum.Parse<BugSchedule>(schedule);
+    //    CaughtStatus = BugCaughtStatus.Unseen;
+    //    TimesCaught = 0;
+    //}
+
+    public void See() {
+        CaughtStatus = BugCaughtStatus.Seen;
+    }
+
+    public void Catch() {
+        CaughtStatus = BugCaughtStatus.Caught;
+        TimesCaught++;
+    }
 }
