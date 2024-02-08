36f55119e8d4d325074f8965f5673911838f66ba
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-08 13:42:41 -0800

    Separate BugDexEntry from BugDex.cs

---
+++ BugKeeper/scripts/BugDexEntry.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+using System;
+
+public class BugDexEntry {
+    public BugDexEntry() {
+        Name = "name";
+        Species = "species";
+        Description = "description";
+        Sprite = new Sprite2D();
+        ActiveTimes = BugActiveTimes.Never;
+        CaughtStatus = BugCaughtStatus.Unseen;
+        TimesCaught = 0;
+    }
+
+    public BugDexEntry(string name, string species, string description, string spritePath, string activeTimes) {
+        Name = name;
+        Species = species;
+        Description = description;
+        Sprite = ResourceLoader.Load<Sprite2D>(spritePath);
+        ActiveTimes = Enum.Parse<BugActiveTimes>(activeTimes);
+        CaughtStatus = BugCaughtStatus.Unseen;
+        TimesCaught = 0;
+    }
+
+    public string Name;
+    public string Species;
+    public string Description;
+    public Sprite2D Sprite;
+    public BugActiveTimes ActiveTimes;
+    public BugCaughtStatus CaughtStatus;
+    public int TimesCaught;
+}
