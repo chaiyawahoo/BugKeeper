56f3023685008655bb966939b351ad09b2a7403a
Chai Nunes <chaiyawahoo@gmail.com>
2024-02-12 01:57:34 -0800

    make bug schedule icons toggleable

--- BugKeeper/scripts/BugDexEntry.cs
+++ BugKeeper/scripts/BugDexEntry.cs
@@ -1,4 +1,5 @@
 ﻿using Godot;
+using Godot.Collections;
 using System;
 
 public enum BugCaughtStatus
@@ -42,21 +43,62 @@ public partial class BugDexEntry : Control
     public string Species;
     [Export]
     public string Description;
+
     [ExportGroup("")]
     [Export]
     public Sprite2D Sprite;
     [Export]
-    public BugSchedule Schedule;
+    public BugSchedule ActiveTimes;
     [Export]
     public BugCaughtStatus CaughtStatus;
+
     public int TimesCaught;
 
-    public void See() {
+    private Node bugScheduleIcons;
+
+
+    public override void _Ready()
+    {
+        TimesCaught = 0;
+        bugScheduleIcons = FindChild("BugSchedule");
+
+        UpdateScheduleIcons();
+    }
+
+    public void See()
+    {
         CaughtStatus = BugCaughtStatus.Seen;
     }
 
-    public void Catch() {
+    public void Catch()
+    {
         CaughtStatus = BugCaughtStatus.Caught;
         TimesCaught++;
+    }
+
+    private void ToggleScheduleIcon(BugSchedule time, bool active)
+    {
+        Control activeIcon = (Control)bugScheduleIcons.FindChild("Active" + time.ToString());
+        Control inactiveIcon = (Control)bugScheduleIcons.FindChild("Inactive" + time.ToString());
+        if (active)
+        {
+            activeIcon.Visible = true;
+            inactiveIcon.Visible = false;
+        }
+        else
+        {
+            inactiveIcon.Visible = true;
+            activeIcon.Visible = false;
+        }
+    }
+
+    private void UpdateScheduleIcons()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            BugSchedule time = (BugSchedule)(1 << i);
+            bool isActive = (ActiveTimes & time) == time;
+            ToggleScheduleIcon(time, isActive);
+        }
     }
 }
